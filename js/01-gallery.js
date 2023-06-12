ce8b17bed7ec41a5895d65c34faf8d6ff9f7648c
azerikt <azerikt@gmail.com>
2023-06-12 16:03:05 +0200

    fix bugs

---
+++ js/01-gallery.js
@@ -0,0 +1,6 @@
+import { galleryItems } from "./gallery-items.js";
+// Change code below this line
+const gallery = document.querySelector(".gallery");
+const liCreator = document.createElement("li");
+
+console.log(galleryItems);
