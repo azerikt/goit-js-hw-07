0e9c737b7d90cdf592cb2e15f4e6c29aac03ff37
azerikt <azerikt@gmail.com>
2023-06-14 17:51:11 +0200

    first commit

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,6 +1,23 @@
 import { galleryItems } from "./gallery-items.js";
 // Change code below this line
-const gallery = document.querySelector(".gallery");
-const liCreator = document.createElement("li");
 
-console.log(galleryItems);
+const gallery = document.querySelector(".gallery");
+
+const liCreator = [];
+
+galleryItems.forEach((el) => {
+  const item = document.createElement("li");
+  item.className = `item`;
+  liCreator.push(item);
+});
+gallery.append(...liCreator);
+
+const list = document.querySelector(".gallery");
+
+const markup = galleryItems
+  .map(
+    (galleryItems) =>
+      `<li><img class="picture" src=${galleryItems.preview} width = '360' alt='${galleryItems.description}'></img></li>`
+  )
+  .join("");
+list.innerHTML = markup;
